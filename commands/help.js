fe1914613e533fc26a6fb2ff434da4253840ef34
flap <60710339+flap02@users.noreply.github.com>
2020-09-15 10:50:53 -1000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,15 +1,15 @@
-const  Discord  =  require ( "discord.js" )
-exportações . run  =  async  ( bot ,  mensagem ,  argumentos ,  arg_texto ,  chat )  =>  {
-  const  ajuda  =  novo  Discord . MessageEmbed ( )
+const Discord = require("discord.js")
+exports.run = async (bot, message, argumentos, arg_texto, chat) => {
+  const ajuda = new Discord.MessageEmbed()
   
-    . setColor ( "VERDE" )
-    . setTitle ( "** Ajuda **" )
-    . setDescription ( `Olá, meu prefixo é! Este é meu centro de comandos, abaixo você pode ver todos meus comandos, separados por categorias` )
-  . addField ( "👮‍♂️ - ** MODERAÇÃO **" ,  "Ban, kick e clear" )
-   . addField ( "📁 - ** UTILIDADE **" ,  "Ajuda, ping, digamos, tíquete e tempo de atividade" )
-  . addField ( "🎉 - ** DIVERSÃO **" ,  "Kiss e coinflip" )
-    . setTimestamp ( )
-    . setFooter ( `Comando solicitado por $ { mensagem . membro . displayName } ` ,  mensagem . autor . displayAvatarURL ( { Tamanho : 32 } ) )   
+    .setColor("GREEN")
+    .setTitle("**Ajuda**")
+    .setDescription(`Olá, meu prefix é ! Este é meu centro de comandos, abaixo você poderá ver todos meus comandos, separados por categorias.`)
+  .addField("👮‍♂️ - **MODERAÇÃO**", "Ban, unban, kick e clear")
+   .addField("📁 - **UTILIDADE**", "Help, ping, say, ticket e uptime")
+  .addField("🎉 - **DIVERSÃO**", "Kiss e coinflip")
+    .setTimestamp()
+    .setFooter(`Comando solicitado por ${message.member.displayName}`, message.author.displayAvatarURL({Size: 32}))   
   
-  mensagem . canal . enviar ( ajuda )
+  message.channel.send(ajuda)
 }
